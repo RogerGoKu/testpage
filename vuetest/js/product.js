8f132b7bb834e0322b4c81c9f17625728c3cf8ea
RogerGoKu <michealwayne666666@gmail.com>
2019-07-26 19:20:38 +0800

    add vuetest

---
+++ vuetest/js/product.js
@@ -0,0 +1,21 @@
+
+	//@@ product @@
+	var $prods = document.querySelectorAll('.m-product');
+	$prods.addEventListener('click', function() {
+		alert('hello');
+	}, false);
+	
+
+	//@@ product @@
+	var $prods = document.querySelectorAll('.m-product');
+	$prods.addEventListener('click', function() {
+		alert('hello');
+	}, false);
+	
+
+	//@@ product @@
+	var $prods = document.querySelectorAll('.m-product');
+	$prods.addEventListener('click', function() {
+		alert('hello');
+	}, false);
+	
