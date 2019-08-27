c00b3d477ba7dd88c0685f14efb675f90a2669b7
RogerGoKu <michealwayne666666@gmail.com>
2019-08-27 22:05:16 +0800

    generator

---
+++ jstest/Generator.js
@@ -0,0 +1,25 @@
+
+function *test1 () {
+	yield 1;
+	yield 'a'
+	yield [123, 456]
+	return 'end'
+}
+
+let it1 = test1();
+
+console.log(it1.next());
+console.log(it1.next());
+console.log(it1.next());
+console.log(it1.next());
+
+function *test2 () {
+	let val = yield setTimeout(() => {
+		it2.next('time out')
+	}, 2000);
+	console.log(val);
+}
+
+let it2 = test2();
+
+it2.next();
