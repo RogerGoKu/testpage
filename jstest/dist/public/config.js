cde96b6755b65a2e89aec9d7c99f762be0f87719
RogerGoKu <michealwayne666666@gmail.com>
2020-06-21 15:29:44 +0800

    test

--- jstest/dist/public/config.js
+++ jstest/dist/public/config.js
@@ -1,4 +1,4 @@
-;(function () {
+/*;(function () {
     window.EMAILS = {
         service: 'aasa@caasa.org.cn',
         cooperation: 'aasa@caasa.org.cn'
@@ -73,4 +73,93 @@
             }
         ]
     }
-} ());
+} ());*/
+"use strict";
+
+;
+
+(function () {
+  window.EMAILS = {
+    service: 'aasa@caasa.org.cn',
+    cooperation: 'aasa@caasa.org.cn'
+  };
+  window.PAGE_CONFIG = {
+    title: '楼澜',
+    pages: [{
+      type: 'blue',
+      title: "\u4E13\u6CE8\u529E\u516C\uFF0C\n                \u505A\u6700\u597D\u7684\u623F\u6E90\u5E73\u53F0",
+      subTitle: '楼澜专注建立一个真实高效的办公房源平台，通过多种数字化营销工具，提升业主和经纪人的工作效率。',
+      others: "<em class=\"u-icon icon-phone_bg g-pa\"></em>\n                <em class=\"u-icon icon-phone g-pa\"></em>"
+    }, {
+      type: 'white',
+      title: "\u6570\u5B57\u65F6\u4EE3\u4E0B\u7684\n                \u62DB\u5546\u9ED1\u79D1\u6280",
+      labels: [{
+        label: '房源曝光',
+        texts: '发布房源，让所有经纪人能找到你和你的房源'
+      }, {
+        label: '智能海报',
+        texts: '随时更新房源信息，自动生成推广海报'
+      }, {
+        label: '推广分析',
+        texts: '追踪推广效果，找出市场推广盲点'
+      }],
+      others: "<em class=\"u-icon u-arrow j-arrow g-pa\"></em>"
+    }, {
+      type: 'blue',
+      title: "\u514D\u8D39\u7684\u623F\u6E90\u5E93\n                +\u4E1A\u4E3B\u901A\u8BAF\u5F55",
+      labels: [{
+        label: '房源搜索',
+        texts: '免费搜索全市场真实房源，直联业主招商'
+      }, {
+        label: '方案制作',
+        texts: '快速生成房源方案，第一时间推给客户'
+      }, {
+        label: '客户管理',
+        texts: '追踪客户看房动态，深入了解客户需求'
+      }],
+      others: "<em class=\"u-icon u-arrow j-arrow g-pa\"></em>"
+    }, {
+      type: 'white',
+      title: "\u4E0D\u6B62\u623F\u6E90\uFF0C\n                \u66F4\u591A\u4F01\u4E1A\u670D\u52A1",
+      labels: [{
+        label: '了解市场',
+        texts: '快速了解租赁市场，租约谈判心中有数'
+      }, {
+        label: '优质服务',
+        texts: '发现优质服务商，设计装修不再踩坑'
+      }, {
+        label: '房源收藏',
+        texts: '收藏心动房源，轻松对比选择'
+      }],
+      others: "<em class=\"u-icon u-arrow j-arrow g-pa\"></em>"
+    }, {
+      type: 'blue',
+      content: {
+        id: 'infos',
+        bg: 'p-demo.png',
+        infos: [{
+          key: 'build',
+          name: '写字楼',
+          value: 1999
+        }, {
+          key: 'garden',
+          name: '园区',
+          value: 2999
+        }, {
+          key: 'work',
+          name: '共享办公',
+          value: 3999
+        }, {
+          key: 'business',
+          name: '商务中心',
+          value: 4999
+        }]
+      }
+    }, {
+      type: 'blue',
+      title: '楼澜愿景',
+      subTitle: '楼澜专注搭建一个开放透明、真实高效的办公房源平台，构建行业的数字化基础设施，通过科技和数据提升办公场景的效率、体验和价值，助推办公场景面向未来的变革。',
+      otherTitles: "<p class=\"u-p2 g-fs28 g-mt60\">Email\uFF1A</p>\n                <p class=\"u-p2 g-fs28 g-mt20\">\u5BA2\u670D\u90AE\u7BB1\uFF1A<a s-cr_white href=\"mailto:".concat(EMAILS.service, "\">").concat(EMAILS.service, "</a><br/>\n                    \u5546\u52A1\u5408\u4F5C\uFF1A<a s-cr_white href=\"mailto:").concat(EMAILS.cooperation, "\">").concat(EMAILS.cooperation, "</a></p>\n                <p class=\"u-p2\"><img src=\"./public/i-qrcode.png\" class=\"qr-code g-mt60\" alt=\"\u697C\u6F9C\u5C0F\u7A0B\u5E8F\u4E8C\u7EF4\u7801\"/></p>")
+    }]
+  };
+})();
